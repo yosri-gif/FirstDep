c59056c9969fcafdac10c82d6111e90ea8233807
yosri-gif <arfaouiyosri977@gmail.com>
2026-06-08 09:04:48 -0400

    Initial commit

--- server/db.js
+++ server/db.js
@@ -8,6 +8,7 @@ export const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 db.connect((err) => {
